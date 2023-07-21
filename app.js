0a41aaadb895d2ee1cd9cf640ec08fb18c7cad5b
Hubeidahsalarmy67 <hubeidahsalarmy@gmail.com>
2023-07-21 21:51:15 +0000

    new changes

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const display = document.querySelector(".display")
 const buttons = document.querySelectorAll("button")
+const specialChars = ["/", "*", "-", "+", "=", "%"]
+let output = "";
+const calculate = (btnValue) => {
+    if(btnValue === "=" && btnValue !== "" ){
+        output = eval(output.replace("%", "/100"));
+    } else if (btnValue === "c" ){
+        output = "";
+    } else if (btnValue === "Del") {
+        output = output.toString().slice(0, -1);
+    } else {
+        if (output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue;
+    }
+    display.value = output;
+};
 
 buttons.forEach((button) => {
-button.addEventListener("click", (e) => console.log(e.target.dataset.value));
+button.addEventListener("click", (e) => console.log((e.target.dataset.value)));
 });
-console.log(display, buttons)
