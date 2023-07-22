4937fd186b31eb2ee623f9c14bc25cd9ecc4cf9c
Hubeidahsalarmy67 <hubeidahsalarmy@gmail.com>
2023-07-22 09:28:53 +0000

    iphone modification

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const display = document.querySelector(".display")
 const buttons = document.querySelectorAll("button")
-const specialChars = ["/", "*", "-", "+", "=", "%"]
+const specialChars = ["&#177", "*", "/", "-", "+", "=", "%"]
 let output = "";
 const calculate = (btnValue) => {
     if(btnValue === "=" && btnValue !== "" ){
         output = eval(output.replace("%", "/100"));
-    } else if (btnValue === "c" ){
+    } else if (btnValue === "AC" ){
         output = "";
-    } else if (btnValue === "Del") {
-        output = output.toString().slice(0, -1);
     } else {
         if (output === "" && specialChars.includes(btnValue)) return;
         output += btnValue;
